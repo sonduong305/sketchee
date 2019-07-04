fe01ce13b5d4b2505574653aeb13bce259327ff4
sonduong305 <sonduong305@gmail.com>
2019-07-04 08:55:54 +0700

    Modify the server to return 5 closest predictions for img

--- server.py
+++ server.py
@@ -60,7 +60,7 @@ def disp_pic():
         img = cv2.resize(img,(28,28))
 
         # make img contains only 0 and 255
-        img = (img>10*(np.ones((img.shape))))*255
+        img = (img>75*(np.ones((img.shape))))*255
         img = np.array(img,dtype = np.int16)
 
         cv2.imwrite("temp\\temp.jpeg",img)
@@ -72,11 +72,12 @@ def disp_pic():
         # cv2.waitKey()
         pred = model.predict(np.expand_dims(img, axis=0))
         
-        ind = (-pred).argsort()[0,0]
+        ind = (-pred).argsort()[0,:5]
         # print(ind)
-        latex = class_names[ind]
-        print(latex)
-        m = {'name': latex}
+        results = [class_names[x] for x in ind]
+        results = [result[:-1] for result in results]
+        # print(latex)
+        m = {'results': results}
     return jsonify(m)
 
 if __name__ == '__main__':
